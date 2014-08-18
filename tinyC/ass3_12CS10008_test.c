17b4921cfbddbd8277b993c42bad322f8520a1d8
Aseem Patni <apatni1993@gmail.com>
2014-08-18 23:44:35 +0530

    added lexer for tinyC

---
+++ tinyC/ass3_12CS10008_test.c
@@ -0,0 +1,14 @@
+/********************************
+**** The best test file ever ****
+********************************/
+int main () { // ass3_12CS10008_test.c
+  int x,i=1;
+  float z67=10.24;
+  char c[] = "Hello";
+  char y2b45dg = 'd';
+  for (i=1;i<10;i++) {
+    if(z67<20.48e-13) {
+      printf ("%s",c);
+    }
+  }
+}
